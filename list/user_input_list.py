d86010686b025994b651776ced36f30944765a2b
Kashish Jaiswal <kashishjais23@gmail.com>
2021-10-06 20:31:04 +0530

    .

--- list/user_input_list.py
+++ list/user_input_list.py
@@ -3,17 +3,13 @@
 # display list values ,min , max ,mean ,average
 # list should be numeric
 x=[] 
-t=0
 size=int(input("enter size of list"))
 for i in range(size+1):
     ele=int(input("enter element"))
     x.append(ele)
-    t+=ele
 print(x)
-avg=t/size
-x_max=max(x)
-x_min=min(x)
-print(f'total is {t}')
-print(f'average is {avg}')
-print(x_max)
-print(x_min)    
+print(max(x))
+print(min(x))
+print('total is', sum(x))
+print('average is',sum(x)/len(x))
+   
