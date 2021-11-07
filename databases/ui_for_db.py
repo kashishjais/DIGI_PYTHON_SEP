21512c69bdc4d45e56699184cf003885eb2fd86f
Kashish Jaiswal <kashishjais23@gmail.com>
2021-11-07 22:33:09 +0530

    updated db

--- databases/ui_for_db.py
+++ databases/ui_for_db.py
@@ -15,23 +15,86 @@ def show_student_form():
 
     if btn and name and klass and section:
         db=open_db()
-        db.add(Student(name=name,section=section,klass=klass)) 
+        db.add(Student(name=name,section=section,klass=klass,is_online=is_online)) 
         db.commit()
         db.close()
         st.success("saved student details")   
 def show_grading_form():
     pass
 
+def show_students_data():
+    db=open_db()
+    student_list=db.query(Student).all()
+    db.close()
+    for student in student_list:
+        with st.expander(f"student detail {student.name}",True):
+            st.subheader(student.name)
+            st.markdown(f'''
+            - class : {student.klass}
+            - section : {student.section}
+            - online : {'✅' if student.is_online else '🚫'}
+            - admit date : {student.admit_date}''')
+            btn=st.button(f"delete{student.name}")
+            if btn:
+                db=open_db()
+                std=db.query(Student).get(student.id)
+                db.delete(std)
+                db.commit()
+                db.close()
+                st.success("deletion completed,press R to refresh list")
+
+def find_student():
+    with st.form("search form"):
+        params=['id','name','section','class']
+        c1,c2=st.columns(2)
+        by=c1.selectbox('Search Student by ',options=params)
+        query=c2.text_input("type keyword here ")
+        btn=st.form_submit_button("search")
+    
+    result=None
+    db=open_db()
+    if btn and query:
+        if by==params[0]:
+             result=db.query(Student).filter(Student.id==query)
+        if by==params[1]:
+             result=db.query(Student).filter(Student.name==query)     
+        if by==params[2]:
+             result=db.query(Student).filter(Student.section==query)
+        if by==params[3]:
+             result=db.query(Student).filter(Student.klass==query)     
+    if result:
+        count=result.count()
+        if count==1:
+            student=result.all()[0]
+            st.info("student details found")
+            st.markdown(f'''
+            ### {student.name}
+            - class : {student.klass}
+            - section : {student.section}
+            - online : {'✅' if student.is_online else '🚫'}
+            - admit date : {student.admit_date}''')
+        elif count>1:
+            for student in result.all():
+                st.markdown(f'''
+                ### {student.name}
+                - class : {student.klass}
+                - section : {student.section}
+                - online : {'✅' if student.is_online else '🚫'}
+                - admit date : {student.admit_date}
+                ----
+                ''')    
+    db.close()
+        
 
 st.title("Database Example for Newbies😎😎")
 
-options=["View students","view grades","add students","add grades"]
+options=["View students","find student","add students","add grades"]
 choice=st.sidebar.radio("select any option",options)
 
 if choice==options[0]:
-    pass
+    show_students_data()
 elif choice==options[1]:
-    pass
+    find_student()
 elif choice==options[2]:
     show_student_form()
 elif choice==options[3]:
